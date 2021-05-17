33ded335bd9abb3d17a97e44e0f353f54b2d2b87
Yashar Zafari <yasharez@gmail.com>
2021-05-16 19:42:31 -0700

    Finished bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,8 +2,8 @@ def bubble_sort(array)
   max_loops = array.length - 1
   for i in 0...array.length - 1
     swapped = false
-    for j in 0..max_loops
-      if array[j + 1] < array[j]
+    for j in 0..max_loops - 1
+      if array[j + 1].to_i < array[j]
         move_back = array[j + 1]
         move_forward = array[j]
         array[j + 1] = move_forward
@@ -12,8 +12,11 @@ def bubble_sort(array)
       end
     end
     max_loops -= 1
-    p max_loops
+    if swapped == false
+      return array
+    end
   end
+  array
 end
 
-bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2])
